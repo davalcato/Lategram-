a7f89bcf42bd62fb242680e76ebcf554fa9a9083
davalcato <davalcato@yahoo.com>
2020-12-01 23:24:16 -0800

    added the database manager to the project

---
+++ Lategram/Resources/DatabaseManager.swift
@@ -0,0 +1,36 @@
+//
+//  DatabaseManager.swift
+//  Lategram
+//
+//  Created by Daval Cato on 12/1/20.
+//
+
+import FirebaseDatabase
+
+public class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    // MARK: - Public
+    
+    public func registerNewUser(username: String)
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
