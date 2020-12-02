a904afcbee6cf2f08c43f823466a3d00624ddc41
davalcato <davalcato@yahoo.com>
2020-12-01 20:16:25 -0800

    first commit

---
+++ Lategram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lategram
+//
+//  Created by Daval Cato on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
