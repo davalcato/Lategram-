e1d59f8a5b27701030b5ddf482f0ac6c5a9c3700
davalcato <davalcato@yahoo.com>
2020-12-06 14:44:28 -0800

    added the private func configurationHeaderView to the LoginView

--- Lategram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Lategram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,18 +9,56 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // putting the cornerRadius numbers into constants...
+    
+    struct Constants {
+        static let cornerRadius: CGFloat = 8.0
+            }
+    
     private let usernameEmailField: UITextField = {
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: 10,
+                                              height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        
+        return field
     }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0,
+                                              y: 0,
+                                              width: 10,
+                                              height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         return field
     }()
     
     private let loginButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        return button
     }()
     
     private let termsButton: UIButton = {
@@ -32,11 +70,20 @@ class LoginViewController: UIViewController {
     }()
     
     private let createAccountButton: UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("New User? Create an Account", for: .normal)
+        
+        return button
+        
     }()
     
     private let headerView: UIView = {
-        return UIView()
+        let header = UIView()
+        header.clipsToBounds = true
+        let backgroundImageView = UIImageView(image: UIImage(contentsOfFile:"gradient"))
+        header.addSubview(backgroundImageView)
+        return header
     }()
     
     
@@ -52,9 +99,19 @@ class LoginViewController: UIViewController {
         
         // assign frames here...
         
-        headerView.frame = CGRect(x: 0, y: view.safeAreaInsets.top,
-                                  width: view.width,
-                                  height: 200)
+        headerView.frame = CGRect(
+            x: 0, y: view.safeAreaInsets.top,
+            width: view.width,
+            height: view.height/3.0)
+        
+        configurationHeaderView()
+    }
+    
+    private func configurationHeaderView() {
+        guard headerView.subviews.count == 1 else {
+            
+            return
+        }
         
     }
     
