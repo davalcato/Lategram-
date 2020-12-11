99576f3df71de39aedc574ccd82f2feaad3d1cde
davalcato <davalcato@yahoo.com>
2020-12-10 19:08:06 -0800

    added a private let searchBar: UISearchBar to the ExploreViewController

--- Lategram/Controllers/Core Tabs/CameraViewController.swift
+++ Lategram/Controllers/Core Tabs/CameraViewController.swift
@@ -5,14 +5,19 @@
 //  Created by Daval Cato on 12/1/20.
 //
 
+import AVFoundation
 import UIKit
 
 class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    private func didTapTakePicture() {
+        
+        
     }
    
 
