234889581d5ee47c6acddc9611e5ef97a4f1cea9
davalcato <davalcato@yahoo.com>
2020-12-16 16:42:20 -0800

    created the spinner in the middle of the view of the notifications center with private let spinner: UIActivityIndicatorView

--- Lategram/Controllers/Core Tabs/NotificationsViewController.swift
+++ Lategram/Controllers/Core Tabs/NotificationsViewController.swift
@@ -13,22 +13,30 @@ class NotificationsViewController: UIViewController, UITableViewDelegate, UITabl
     
     private let tableView: UITableView = {
         let tableView = UITableView()
+        tableView.isHidden = true
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         return tableView
         
     }()
     
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.tintColor = .label
+        return spinner
+    }()
+    
     private lazy var noNotificationsView = NoNotificationsView()
-        
- 
-
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Notifications"
         view.backgroundColor = .systemBackground
-//        view.addSubview(tableView)
-        view.addSubview(noNotificationsView)
+        spinner.startAnimating()
+        // Add a subview to make the request
+        view.addSubview(spinner)
+        view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
         
@@ -37,11 +45,24 @@ class NotificationsViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+        spinner.frame = CGRect(x: 0,
+                               y: 0,
+                               width: 100,
+                               height: 100)
+        spinner.center = view.center
+        
+    }
+    
+    //
+    private func addNoNotificationView() {
+        tableView.isHidden = true
+        view.addSubview(tableView)
         noNotificationsView.frame = CGRect(x: 0,
                                            y: 0,
                                            width: view.width/2,
-                                           height: view.width/2)
+                                           height: view.width/4)
         noNotificationsView.center = view.center
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
