771d63bb4d47d6686b48863a99797aee8b2e4e48
davalcato <davalcato@yahoo.com>
2020-12-19 21:46:04 -0800

    Created model HomeFeedRenderViewModel to encapsule four view models

--- Lategram/Controllers/Core Tabs/HomeViewController.swift
+++ Lategram/Controllers/Core Tabs/HomeViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import FirebaseAuth
 
-// Struct will wrap four of the render view models
+// Created model HomeFeedRenderViewModel to encapsule four view models
 struct HomeFeedRenderViewModel {
     let header: PostRenderViewModel
     let post: PostRenderViewModel
@@ -65,20 +65,140 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let x = section
+        // Determining which model the user wants
+        let model: HomeFeedRenderViewModel
+        if x == 0 {
+             model = feedRenderModels[0]
+        }
+        else {
+            // This gives the position of the model that we want
+            let position = x % 4 == 0 ? x/4 : ((x - (x % 4)) / 4)
+             model = feedRenderModels[position]
+        }
+        
+        // Determining which position the user is at
+        let subSection = x % 4
+        
+        if subSection == 0 {
+            // header
+            return 1
+        }
+        else if subSection == 1 {
+            // post
+            return 1
+        }
+        else if subSection == 2 {
+            // actions
+            return 1
+        }
+        else if subSection == 3 {
+            // comment
+            // Get the comment model out of the primary model
+            let commentsModel = model.comments
+            
+            switch commentsModel.renderType {
+            case .comments(let comments): return comments.count > 2 ? 2 : comments.count
+            case .header, .actions, .primaryContent: return 0
+            
+            }
+        }
         return 0
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+        let x = indexPath.section
+        // Determining which model the user wants
+        let model: HomeFeedRenderViewModel
+        if x == 0 {
+             model = feedRenderModels[0]
+        }
+        else {
+            // This gives the position of the model that we want
+            let position = x % 4 == 0 ? x/4 : ((x - (x % 4)) / 4)
+             model = feedRenderModels[position]
+        }
         
-        return cell
+        let subSection = x % 4
+        
+        if subSection == 0 {
+            // header
+            let headerModel = model.header
+            switch headerModel.renderType {
+            case .header(let user):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier, for: indexPath) as! IGFeedPostHeaderTableViewCell
+                return cell
+            case .comments, .actions, .primaryContent: return UITableViewCell()
+            }
+        }
+        else if subSection == 1 {
+            // post
+            let postModel = model.post
+            switch postModel.renderType {
+            case .primaryContent(let post):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+                return cell
+            case .comments, .actions, .header: return UITableViewCell()
+                
+            }
+        }
+        else if subSection == 2 {
+            // actions
+            let actionSection = model.actions
+            switch actionSection.renderType {
+            case .actions(let provider):
+                // Configure the cell views
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier, for: indexPath) as! IGFeedPostActionsTableViewCell
+                return cell
+            case .comments, .header, .primaryContent: return UITableViewCell()
+            }
+        }
+        else if subSection == 3 {
+            // Get the comment model out of the primary model
+            let commentModel = model.comments
+            switch commentModel.renderType {
+            case .comments(let comments):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
+                return cell
+            case .header, .actions, .primaryContent: return UITableViewCell()
+            }
+            
+        }
+            return UITableViewCell()
+            
+       
     }
     
-}
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return feedRenderModels.count * 4
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let subSection = indexPath.section % 4
+        if subSection == 0 {
+            // Return the height for the header
+            return 70
+        }
+        else if subSection == 1 {
+            return tableView.width
+            
+        }
+        else if subSection == 2 {
+            return 60
+            
+        }
+        else if subSection == 3 {
+            return 50
+        }
+            return 0
+        }
+    }
+
 
