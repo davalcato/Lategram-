295e91433c0b83864cec5d0dde1e6fb3b86ba34a
davalcato <davalcato@yahoo.com>
2020-12-21 20:19:16 -0800

    Return a basic UIView for the divider with a viewForFooterInSection on the HomeViewController

--- Lategram/Controllers/Core Tabs/HomeViewController.swift
+++ Lategram/Controllers/Core Tabs/HomeViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import FirebaseAuth
 
-// Created model HomeFeedRenderViewModel to encapsule four view models
+// Created model HomeFeedRenderViewModel to encapsule four submodels
 struct HomeFeedRenderViewModel {
     let header: PostRenderViewModel
     let post: PostRenderViewModel
@@ -116,10 +116,11 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         // Determining which model the user wants
         let model: HomeFeedRenderViewModel
         if x == 0 {
+            // Get the model out of the collection - if 0 then get the first one
              model = feedRenderModels[0]
         }
         else {
-            // This gives the position of the model that we want
+            // Otherwise how many times does 4 go into x
             let position = x % 4 == 0 ? x/4 : ((x - (x % 4)) / 4)
              model = feedRenderModels[position]
         }
@@ -127,6 +128,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         // Determining which position the user is at
         let subSection = x % 4
         
+        // if its the header case then only 1 row
         if subSection == 0 {
             // header
             return 1
@@ -145,11 +147,13 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             let commentsModel = model.comments
             
             switch commentsModel.renderType {
+            // If theres more then two comments then max it out to two
             case .comments(let comments): return comments.count > 2 ? 2 : comments.count
             case .header, .actions, .primaryContent: return 0
             
             }
         }
+        // If all else fails then return 0
         return 0
     }
     
@@ -169,20 +173,18 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         let subSection = x % 4
         
         if subSection == 0 {
-            // header
-            let headerModel = model.header
-            switch headerModel.renderType {
+            // header is driving the renderType
+            switch model.header.renderType {
             case .header(let user):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier, for: indexPath) as! IGFeedPostHeaderTableViewCell
                 return cell
-                // Return a cell instead of a number UITableViewCell
+                // If we're looking at the header cell we shouldn't see [cooments, actions, etc]
             case .comments, .actions, .primaryContent: return UITableViewCell()
             }
         }
         else if subSection == 1 {
             // post
-            let postModel = model.post
-            switch postModel.renderType {
+            switch model.post.renderType {
             case .primaryContent(let post):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
                 return cell
@@ -192,8 +194,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         }
         else if subSection == 2 {
             // actions
-            let actionSection = model.actions
-            switch actionSection.renderType {
+            switch model.actions.renderType {
             case .actions(let provider):
                 // Configure the cell views
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier, for: indexPath) as! IGFeedPostActionsTableViewCell
@@ -203,8 +204,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         }
         else if subSection == 3 {
             // Get the comment model out of the primary model
-            let commentModel = model.comments
-            switch commentModel.renderType {
+            switch model.comments.renderType {
             case .comments(let comments):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
                 return cell
@@ -224,7 +224,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+    // Based on the subsections we're return the height
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let subSection = indexPath.section % 4
         if subSection == 0 {
@@ -232,25 +232,29 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             return 70
         }
         else if subSection == 1 {
+            // Post
             return tableView.width
             
         }
         else if subSection == 2 {
+            // Actions (like/comment)
             return 60
             
         }
         else if subSection == 3 {
+            // Comment row
             return 50
         }
             return 0
     }
-    
+    // Return a basic UIView for the divider
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        // No subview here
         return UIView()
     }
-    
+    // Return the height for the footer
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        // Return at every fourth section
+        // Because the subsection is the comments add a buffer zone of 70 pixels
         let subSection = section % 4
         return subSection == 3 ? 70 : 0
     }
