33d60ad4154d5a386968fa99342dac8ee0657c26
davalcato <davalcato@yahoo.com>
2020-12-23 21:24:02 -0800

    Consolidated the searchbar in the ExploreViewController with private func configureSearchbar()

--- Lategram/Controllers/Core Tabs/ExploreViewController.swift
+++ Lategram/Controllers/Core Tabs/ExploreViewController.swift
@@ -28,6 +28,9 @@ class ExploreViewController: UIViewController {
     // Add a collectView here...
     private var collectionView: UICollectionView?
     
+    // Creating another collectioView here
+    private var tabbedSearchCollection: UICollectionView?
+    
     // Create the dim view
     private let dimmedView: UIView = {
         // Black view
@@ -41,34 +44,12 @@ class ExploreViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.topItem?.titleView = searchBar
         
-        // create the UICollectionView here...
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.scrollDirection = .vertical
-        
-        // Three column set up
-        layout.itemSize = CGSize(width: (view.width-4)/3, height: (view.width-4)/3)
-        layout.minimumLineSpacing = 1
-        layout.minimumInteritemSpacing = 1
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
-        // Registering cells here
-        collectionView?.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
-        
-        
-        collectionView?.delegate = self
-        collectionView?.dataSource = self
-        guard let collectionView = collectionView else {
-            
-            return
-        }
-        
-        view.addSubview(collectionView)
-        
-        // Assigned the SearchBar delegate to get some view action
-        searchBar.delegate = self
+        // Consolidating the searchbar here
+        configureSearchbar()
+      
+        // Configure the tabbedSearchCollection
+        configureExploreCollection()
         
         // Add after the collectionView so it sits on top
         view.addSubview(dimmedView)
@@ -80,8 +61,34 @@ class ExploreViewController: UIViewController {
         
         // Added this gesture to the dim view
         dimmedView.addGestureRecognizer(gesture)
+    }
+    // Consolidating the searchbar here
+    private func configureSearchbar() {
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        searchBar.delegate = self
         
     }
+    
+    // Make the configureExploreCollection its own function
+    private func configureExploreCollection() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: (view.width-4)/3, height: (view.width-4)/3)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        guard let collectionView = collectionView else {
+            
+            return
+        }
+        view.addSubview(collectionView)
+        
+    }
+    
     
     // Assigned frames to the search collectionView
     override func viewDidLayoutSubviews() {
