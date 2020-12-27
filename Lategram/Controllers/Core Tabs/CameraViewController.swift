ba79b64df23a3b7e11a3703502420687061b7b64
davalcato <davalcato@yahoo.com>
2020-12-26 19:39:18 -0800

    Added the  @IBAction func didTapTakePicture CameraViewController and the extension UIImagePickerControllerDelegate, UINavigationControllerDelega

--- Lategram/Controllers/Core Tabs/CameraViewController.swift
+++ Lategram/Controllers/Core Tabs/CameraViewController.swift
@@ -16,18 +16,77 @@ class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Change imageView background
+        imageView.backgroundColor = .secondarySystemBackground
         navigationController?.setNavigationBarHidden(true, animated: false)
         
         // Button color
         button.backgroundColor = .systemBlue
         button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
         
     }
     
     @IBAction func didTapTakePicture() {
         
+//        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            let alertController = UIAlertController(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+//
+//            let okAction = UIAlertAction(title: "Alright", style: .default, handler: { (alert: UIAlertAction!) in
+//            })
+//
+//            alertController.addAction(okAction)
+//            self.present(alertController, animated: true, completion: nil)
+//        } else {
+//            // Other action
+//        }
         
+        // Presenting camera here
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        
+        // Assign picker delegate to self
+        picker.delegate = self
+        present(picker, animated: true)
     }
-   
-
+    
 }
+
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Picker dismis
+        picker.dismiss(animated: true, completion: nil)
+        
+        
+        // Taking a reference of the image
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+        UIImage else {
+                return
+        }
+        // Once we have an image
+        imageView.image = image
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
